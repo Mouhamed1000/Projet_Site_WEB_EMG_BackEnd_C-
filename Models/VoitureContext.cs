e595b935acd055a8d978cb2442a9a52d80f7ce5d
Mouhamed1000 <ndoyenaby12@gmail.com>
2025-01-02 00:09:04 +0000

    Installation de EF Core et Pomelo EF Core, création d'une classe héritant de DbContext afin de pourvoir interagir avec notre base de données

---
+++ Models/VoitureContext.cs
@@ -0,0 +1,35 @@
+using EMG_MED1000_BACKEND.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMG_MED1000_BACKEND.Models
+{
+    //Création d'une classe VoitureContext héritant de DbContext
+    //Cette classe servira à l'interaction avec la base de données en utilisant EF Core
+    public class VoitureContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        //Constructeur
+        public VoitureContext(DbContextOptions<VoitureContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        //entitées
+        private DbSet<Voiture> Voitures;
+        private DbSet<Marque> Marques;
+        private DbSet<Modele> Modeles;
+
+        //Implémentation de la méthode OnConfiguring (...) permettant l'import des configs de notre base de données
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    } 
+}
